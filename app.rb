1349881ee242ce0d9e15fa77f373e6642e7c603c
Kirill Sukhorukov <kirill.suhorukov@gmail.com>
2023-11-20 01:42:19 +0800

    scaf

---
+++ app.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+
+require 'pry'
+
+class MagicDigits
+  def initialize
+  end
+
+  def calculate_sum 
+  end
+
+  def do
+  end 
+
+  def odd?(n)
+    binding.pry
+    n.odd?
+  end
+end 
+
+MagicDigits.new.do
